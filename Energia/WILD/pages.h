e7f12df4bce4e658758ed803b4b0eddd22129583
furdozy <calebphillips211@gmail.com>
2019-07-15 16:12:53 -0400

    Update pages.h

--- Energia/WILD/pages.h
+++ Energia/WILD/pages.h
@@ -1,7 +1,7 @@
 #define  VIEW  "        top           unread:0 total:0  <  View Messages   >       bottom       "
   //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        >              
-#define SEND   "                    <   Send Message   >                                        "
-                
-#define OPT    "                    \n<     Options      >\n                    \n                    \n"
-                
-#define SOS    "                    \n<    Send  SOS     >\n                    \n                    \n"
+#define SEND   "                                        <   Send Message   >                    "
+  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        >                     
+#define OPT    "                                        <     Options      >                    "
+  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        >                    
+#define SOS    "                                        <    Send  SOS     >                    "
