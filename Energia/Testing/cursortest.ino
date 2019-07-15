8e4a9cc8a7c54881d8b6d6587e111b6dc2118574
bbrinston <39634310+bbrinston@users.noreply.github.com>
2019-07-15 16:41:30 -0400

    Update cursortest.ino

--- Energia/Testing/cursortest.ino
+++ Energia/Testing/cursortest.ino
@@ -1,46 +1,3 @@
-/*
-  LiquidCrystal Library - Hello World
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
- The circuit:
- =================================
- LCD pin              Connect to
- ---------------------------------
- 01 - GND             GND, pot
- 02 - VCC             +5V, pot
- 03 - Contrast        Pot wiper
- 04 - RS              Pin8 (P2.0) 4.4
- 05 - R/W             GND
- 06 - EN              Pin9 (P2.1) 4.5
- 07 - DB0             GND
- 08 - DB1             GND
- 09 - DB2             GND
- 10 - DB3             GND
- 11 - DB4             Pin10 (P2.2) 4.0
- 12 - DB5             Pin11 (P2.3) 4.1
- 13 - DB6             Pin12 (P2.4) 4.2
- 14 - DB7             Pin13 (P2.5) 4.3
- 15 - BL+             +5V
- 16 - BL-             GND
- =================================
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
 
 // include the library code:
 #include <LiquidCrystal.h>
@@ -80,8 +37,8 @@ void init_cursor()
   pinMode(centercur, INPUT_PULLUP); // High-Z
 
   attachInterrupt(digitalPinToInterrupt(acur), up, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(bcur), down, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ccur), left, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(bcur), left, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ccur), down, CHANGE);
   attachInterrupt(digitalPinToInterrupt(dcur), right, CHANGE);
   attachInterrupt(digitalPinToInterrupt(centercur), center, CHANGE);
 }
@@ -98,35 +55,40 @@ void setup() {
 }
 
 void loop() {
-    
+    interrupts();
 }
 
 void up()
 {
+  noInterrupts();
   if(digitalRead(acur) == 0)
     lcd.print("UP");
 }
 
-void down()
+void left()
 {
+  noInterrupts();
   if(digitalRead(bcur) == 0)
     lcd.print("LEFT");
 }
 
-void left()
+void down()
 {
+  noInterrupts();
   if(digitalRead(ccur) == 0)
     lcd.print("DOWN");
 }
 
 void right()
 {
+  noInterrupts();
   if(digitalRead(dcur) == 0)
     lcd.print("RIGHT");
 }
 
 void center()
 {
+  noInterrupts();
   if(digitalRead(centercur) == 0)
     lcd.print("CENTER");
 }
