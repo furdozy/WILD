abcf52b3f1d56f31d6ee81845ee9fa6d5c6aec70
furdozy <calebphillips211@gmail.com>
2019-07-25 10:23:38 -0400

    Update pages.h

--- Energia/WILD/pages.h
+++ Energia/WILD/pages.h
@@ -1,13 +1,13 @@
-#define  VIEW  "                                        <  View Messages   >                    "
-  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        >              
-#define SEND   "                                        <   Send Message   >                    "
-  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        >                     
-#define OPT    "                                        <     Options      >                    "
-  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        >                    
-#define SOS    "                                        <    Send  SOS     >                    "
-  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        >                    
-#define INFO   "Wilderness Info     Designed by UCF SD  Device              Group 10 Summer 2019"
-  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        > 
-#define ADD    "Select a device to                      add:                <     blank        >"
-  //row         <     row 0        ><     row 2        ><     row 1        ><     row 3        > 
-#define EXIT   "                                               Exit                             "
+#define  VIEW  "                    <  View Messages   >                                        "
+  //row         <     row 0        ><     row 1        ><     row 2        ><     row 3        >              
+#define SEND   "                    <   Send Message   >                                        "
+  //row         <     row 0        ><     row 1        ><     row 2        ><     row 3        >                    
+#define OPT    "                    <     Options      >                                        "
+  //row         <     row 0        ><     row 1        ><     row 2        ><     row 3        >                   
+#define SOS    "                    <    Send  SOS     >                                        "
+  //row         <     row 0        ><     row 1        ><     row 2        ><     row 3        >                   
+#define INFO   "Wilderness Info     Device              Designed by UCF SD  Group 10 Summer 2019"
+  //row         <     row 0        ><     row 1        ><     row 2        ><     row 3        >
+#define ADD    "Select a device to  add:                <     blank        >                    "
+  //row         <     row 0        ><     row 1        ><     row 2        ><     row 3        >
+#define EXIT   "                           Exit                                                 "
